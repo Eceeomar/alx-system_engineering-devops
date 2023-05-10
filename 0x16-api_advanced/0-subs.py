d83e5f5d143ce796d65d95006f3a9543d5636cce
Eceeomar <isiomaenuonye@gmail.com>
2023-05-10 02:38:42 -0700

    fixed task 0 error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers"""
 
+""
+"Returns the number of subscribers"
+""
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+	import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
+# Send a GET request to the Reddit API to fetch subreddit information
+sub_info = requests.get(f "https://www.reddit.com/r/{subreddit}/about.json",
+	headers = { "User-Agent": "My-User-Agent" },
+	allow_redirects = False)
 
-    return sub_info.json().get("data").get("subscribers")
+# If the request was unsuccessful or subreddit doesn't exist, return "OK"
+if sub_info.status_code != 200 or sub_info.json().get("error") == 404:
+	return "OK"
+
+# Extract the number of subscribers from the JSON response
+return sub_info.json().get("data").get("subscribers")
